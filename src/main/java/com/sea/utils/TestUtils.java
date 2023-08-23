e7bad4b0dfe2759efa9af6ad30282a1da85687c7
GentleThug <wu466687121@qq.com>
2023-08-23 14:27:00 +0800

    test1

---
+++ src/main/java/com/sea/utils/TestUtils.java
@@ -0,0 +1,15 @@
+package com.sea.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author: WGH
+ * @Date: 2023-08-23  14:26
+ * @Description:
+ */
+@Slf4j
+public class TestUtils {
+    public void getHelloWorld(){
+        log.info("Hello World");
+    }
+}
